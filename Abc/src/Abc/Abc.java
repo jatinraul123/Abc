4a76e70500519ae686104a7a53981c205f8aac91
sdac <sdac@sdac-PC>
2023-06-01 02:49:38 +0530

    update 2

--- Abc/src/Abc/Abc.java
+++ Abc/src/Abc/Abc.java
@@ -4,6 +4,7 @@ public class Abc {
 	public static void main(String[] args) {
 		System.out.println("update 1.0.0");
 		System.out.println("update1.0.1");
+		System.out.println("update 2.0");
 	}
 
 }
